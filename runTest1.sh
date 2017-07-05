a1d397d61e51bb508abd0d43547985c6bd640dc2
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-07-05 11:08:15 +0100

    Unifies tests in single file

--- runTest1.sh
+++ runTest1.sh
@@ -17,4 +17,19 @@ if [ ! -f out_exchanged.csv ]; then
    	exit 1
 fi
 
+wget "https://drive.google.com/uc?export=download&id=0B1lAg6jyw6lvTWttbnUtUDl0RE0" -O multipeak.csv 
+wget "https://drive.google.com/uc?export=download&id=0B1lAg6jyw6lvNTdCbmQ0Z2ZOTkk" -O multi.zip 
+
+runRamid.R -i multipeak.csv -o out_multipeak.csv -z multi.zip
+rc=$?; 
+if [[ $rc != 0 ]]; then 
+	echo "R process failed with error $rc"
+	exit $rc; 
+fi
+
+if [ ! -f out_multipeak.csv ]; then
+   	echo "File out_multipeak.csv does not exist, failing test."
+   	exit 1
+fi
+
 echo "ramid runs with test data without error codes, all expected files created."
